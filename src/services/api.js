acbb62ac08787739f3da5fea9b40dfda3baec4b1
Duvhier <duvier.tavera01@unicatolica.edu.co>
2025-11-20 23:42:07 -0500

    Fix api.js Profile

--- src/services/api.js
+++ src/services/api.js
@@ -54,7 +54,7 @@ export const productsAPI = {
 export const authAPI = {
   register: (data) => api.post('/auth/register', data),
   login: (data) => api.post('/auth/login', data),
-  getMe: () => api.get('/auth/profile'),
+  getMe: () => api.get('/auth/me'),
   forgotPassword: (data) => api.post('/auth/forgot-password', data),
   resetPassword: (token, data) => api.post(`/auth/reset-password/${token}`, data),
 };
